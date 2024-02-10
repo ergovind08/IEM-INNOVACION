8a0ef4eb5cdd34d899f223d1f9be7821dc5b119c
GOVIND JHA <govindjha@GOVINDs-MacBook-Air.local>
2024-02-10 14:46:22 +0530

    update

--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -16,11 +16,11 @@ const Card = () => {
           <div className="flex flex-col justify-center w-full p-8">
             <div className="bg-gradient-to-r from-slate-700 to-slate-500 rounded-t-md md:rounded-l-md p-4">
               <h3 className="inline-flex font-Consolas items-center text-3xl font-semibold text-white">
-                Event{/* Event <ArrowUpRight className="ml-2 h-8 w-8" /> */}
+                Innovación 2024
               </h3>
             </div>
-            <div className="mt-4 text-lg font-Consolas text-gray-400 overflow-auto max-w-[700px]">
-              <h4 className="font-Consolas text-rigth ">
+            <div className="mt-4 text-lg  text-gray-400 overflow-auto max-w-[700px]">
+              <h4 className=" text-rigth ">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Excepturi, debitis? Lorem ipsum dolor sit amet consectetur
                 adipisicing elit. Excepturi, debitis? Lorem ipsum dolor sit amet
@@ -38,53 +38,12 @@ const Card = () => {
                 dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
               </h4>
             </div>
-            <div className="mt-4">
-              <button className="inline-block px-3 py-3 text-sm font-semibold text-white bg-gradient-to-r from-blue-400 to-blue-600 rounded-full hover:from-blue-500 hover:to-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
-                Vist Now <ArrowUpRight className="ml-2 h-8 w-8" />
+            <div className="mt-4 flex-row">
+              <button className=" flex flex-row px-3 py-2 align-center text-sm font-semibold text-white bg-gradient-to-r from-blue-900 to-red-600 rounded-2xl hover:from-blue-500 hover:to-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
+                <p className="text-xl shadow-2xl">Vist Now</p>
+                <ArrowUpRight className="ml-2 h-8 w-8" />
               </button>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex w-5xl h-[600px] flex-col-reverse items-center justify-center rounded-md md:flex-row shadow-lg transform ">
-          <div className="flex flex-col justify-center w-full p-8">
-            <div className="bg-gradient-to-r from-slate-700 to-slate-500 rounded-t-md md:rounded-l-md p-4">
-              <h3 className="inline-flex font-Consolas items-center text-3xl font-semibold text-white">
-                Event{/* Event <ArrowUpRight className="ml-2 h-8 w-8" /> */}
-              </h3>
-            </div>
-            <div className="mt-4 text-lg font-Consolas text-gray-400 overflow-auto max-w-[700px]">
-              <h4 className="font-Consolas text-left ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi, debitis? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Excepturi, debitis? Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Excepturi, debitis? Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi, debitis? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Excepturi, debitis? Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Excepturi, debitis? Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi, debitis? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Excepturi, debitis? Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Excepturi, debitis? Lorem ipsum
-                dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
-              </h4>
-            </div>
-            <div className="mt-4">
-              <button className="inline-block px-3 py-3 text-sm font-semibold text-white bg-gradient-to-r from-blue-400 to-blue-600 rounded-full hover:from-blue-500 hover:to-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
-                Vist Now <ArrowUpRight className="ml-2 h-8 w-8" />
-              </button>
-            </div>
-          </div>
-          <div className="h-full lg:w-[500px]  lg:h-[500px] w-full md:h-[200px] md:w-[300px] overflow-hidden rounded-b-md md:rounded-r-md md:rounded-b-none">
-            <img
-              src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              alt="Laptop"
-              className="h-full w-full rounded-5xl object-cover"
-            />
           </div>
         </div>
       </div>
