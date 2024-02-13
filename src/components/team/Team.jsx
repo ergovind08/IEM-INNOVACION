4f1202d459e7bb2248e2ef1062b0f3a8eda55551
Yashgupta9330 <yaahg342@gmail.com>
2024-02-14 03:27:59 +0530

    event 2nd commit

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -5,12 +5,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./team.css"
 import Footer from "../footer/Footer.jsx";
-import MyCard from "./Card.jsx";
 import { motion } from "framer-motion";
+import Card from "./Card.jsx";
 
 const Team = () => {
 
-  const [member, setMember] = useState([]);
+  const [member, setMember] = useState(teamData[0].members);
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const handleTeamClick = (teamMembers) => {
@@ -58,32 +58,32 @@ const Team = () => {
 
   return (
     <div className='absolute top-0 w-screen h-screen mt-20 overflow-auto'>
-    <div className='w-full h-[350px] p-1 bg-[#111111] bg-opacity-70 '>
-    <h2 className='w-full my-8 text-3xl italic font-bold text-center'>TEAM</h2>
+    <div className='w-full h-[250px]  bg-[#111111] bg-opacity-70 '>
+    <h2 className='w-full my-10 text-3xl italic font-bold text-center'>TEAM</h2>
     <div className="w-20 h-1 mx-auto mb-[50px] -mt-4 text-gray-100 bg-gray-100 rounded" ></div>
-    <div className='w-3/4 m-auto h-[200px]'>
+    <div className='w-2/4 m-auto h-[150px]'>
     <Slider {...settings}>
       {teamData.map((item, index) => (
-         <div key={index}className="h-full cursor-pointer">
-          <div
-            className={`box transition-transform ${
-                hoveredIndex === index ? "scale-120" : ""
-              }`}
-              onClick={() => handleTeamClick(item.members)}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-          >{item.teamName}
-          </div>
-          </div>
+          <div key={index} className="box flex items-center justify-center transform transition-transform hover:scale-110 cursor-pointer">
+          <button
+            onClick={() => handleTeamClick(item.members)}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+            className="mx-12 my-2 "
+          >
+            {item.teamName}
+          </button>
+        </div>
+        
       ))}
      </Slider>
     </div>
     </div>
-    <div className="mb-[100px]  pt-9 ml-10 text-white mt-[100px] overflow-auto">
+    <div className="mb-[100px]  pt-5 ml-10 text-white mt-[100px] overflow-auto">
       <div className="flex flex-wrap justify-center">
         {member.map((item,index) => (
           <div className="w-1/3 px-8 rounded-2xl" key={index}>
-            <MyCard content={item} />
+            <Card content={item}/>
           </div>
         ))}
       </div>
